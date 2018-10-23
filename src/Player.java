79a26fff24dde46762edbb148413699fe0693ad6
garnacpj <garnacpj@garnacpj-5520.rose-hulman.edu>
2018-10-23 14:00:53 -0400

    10/23

--- src/Player.java
+++ src/Player.java
@@ -13,6 +13,7 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 import java.awt.geom.Point2D;
+
 /**
  * 
  * HI
@@ -28,11 +29,9 @@ public class Player extends JComponent {
 
 	private Point2D position;
 	private Point2D velocity;
-<<<<<<< HEAD
-	final double g = -0.5;
-=======
+	final double g = 0.5;
 	private ArrayList<Shape> borders;
->>>>>>> branch 'master' of https://github.com/parzival111/BubbleBobble.git
+
 	final double movementSpeed = 3;
 	final int WIDTH = 10;
 	final int HEIGHT = 10;
@@ -40,6 +39,7 @@ public class Player extends JComponent {
 	public Player() {
 		position = new Point2D.Double(WIDTH / 2, FRAME_HEIGHT - WIDTH / 2);
 		velocity = new Point2D.Double(0, 0);
+
 		borders = new ArrayList<Shape>();
 		LevelLoader levelLoader = new LevelLoader();
 		levelLoader.loadLevel("Level1");
@@ -50,13 +50,17 @@ public class Player extends JComponent {
 		switch (keyPress) {
 		case 39:
 			// Move Right
-			System.out.println("Move Right");
-			this.updateVelocity((int) this.movementSpeed);
+			if (this.velocity.getX() >= 0) {
+				System.out.println("Move Right");
+				this.updateVelocity((int) this.movementSpeed, false);
+			}
 			break;
 		case 37:
 			// Move Left
-			System.out.println("Move Left");
-			this.updateVelocity((int) -this.movementSpeed);
+			if (this.velocity.getX() >= 0) {
+				System.out.println("Move Left");
+				this.updateVelocity((int) -this.movementSpeed, false);
+			}
 			break;
 		case 38:
 			// Jump
@@ -68,70 +72,73 @@ public class Player extends JComponent {
 			break;
 		default:
 			System.out.println("Handling Key Press " + keyPress);
-			this.updateVelocity(0);
+			this.updateVelocity(0, false);
 			this.updatePosition();
 			break;
 		}
 
 	}
-	
+
 	public void handleKeyRelease(int keyPress) {
 		switch (keyPress) {
 		case 39:
 			// Move Right
 			System.out.println("Move Right");
-			this.updateVelocity((int) this.movementSpeed);
+			this.updateVelocity(0, true);
 			break;
 		case 37:
 			// Move Left
 			System.out.println("Move Left");
-			this.updateVelocity((int) -this.movementSpeed);
+			this.updateVelocity(0, true);
 			break;
 		case 38:
 			// Jump
-			System.out.println("Jump");
-			this.jump();
 			break;
 		case 0:
+			this.updateVelocity(0, true);
 			this.updatePosition();
 			break;
 		default:
 			System.out.println("Handling Key Press " + keyPress);
-			this.updateVelocity(0);
+			this.updateVelocity(0, false);
 			this.updatePosition();
 			break;
 		}
 	}
-	
+
 	public void timePassed() {
 		this.updateVelocity(0, false);
 		this.updatePosition();
 	}
-	
-	public void updateVelocity(int v) {
-		
-		this.velocity.setLocation(v, this.velocity.getY() + g);
+
+	public void updateVelocity(int v, boolean stopped) {
+		if (stopped) {
+			this.velocity.setLocation(0, this.velocity.getY());
+		} else if (v == 0) {
+			this.velocity.setLocation(this.velocity.getX(), this.velocity.getY() + g);
+		} else {
+			this.velocity.setLocation(v, this.velocity.getY() + g);
+		}
 	}
 
 	public void updatePosition() {
-<<<<<<< HEAD
-		CheckBoundary();
-		this.position.setLocation(this.position.getX() + this.velocity.getX(),
-				this.position.getY() + this.velocity.getY());
-=======
+
 		double pXNew = this.position.getX() + this.velocity.getX();
+
 		if (this.position.getX() >= FRAME_WIDTH - WIDTH / 2 && this.velocity.getX() > 1) {
 			pXNew = FRAME_WIDTH - WIDTH / 2;
 			this.velocity.setLocation(0, this.velocity.getY());
 		}
 		if (this.position.getX() <= 0 + WIDTH / 2 && this.velocity.getX() < 1) {
-			pXNew = WIDTH/2;
+			pXNew = WIDTH / 2;
 			this.velocity.setLocation(0, this.velocity.getY());
 		}
+
 		double pYNew = this.position.getY() + this.velocity.getY();
-		if (this.position.getY() > FRAME_HEIGHT - WIDTH/2) pYNew = FRAME_HEIGHT - WIDTH/2;
+		if (this.position.getY() >= FRAME_HEIGHT - WIDTH / 2 && this.velocity.getY() > 0)
+			pYNew = FRAME_HEIGHT - WIDTH / 2;
+
 		this.position.setLocation(pXNew, pYNew);
->>>>>>> branch 'master' of https://github.com/parzival111/BubbleBobble.git
 
 	}
 
@@ -142,27 +149,15 @@ public class Player extends JComponent {
 		}
 	}
 
-<<<<<<< HEAD
-	public void CheckBoundary() {
-		if (this.position.getX() == FRAME_WIDTH - WIDTH / 2 && this.velocity.getX() > 1) {
-			this.velocity.setLocation(0, this.velocity.getY());
-		}
-		if (this.position.getX() == 0 + WIDTH / 2 && this.velocity.getX() < 1) {
-			this.velocity.setLocation(0, this.velocity.getY());
-		}
-	}
-
-=======
->>>>>>> branch 'master' of https://github.com/parzival111/BubbleBobble.git
 	public boolean checkFloor() {
 		return this.position.getY() >= FRAME_HEIGHT - WIDTH / 2;
 	}
 
 	public void drawOn(Graphics2D g2) {
 		g2.setColor(Color.BLACK);
-		g2.fillRect((int) this.position.getX()-WIDTH, (int) this.position.getY()-WIDTH, WIDTH, HEIGHT);
+		g2.fillRect((int) this.position.getX() - WIDTH, (int) this.position.getY() - WIDTH, WIDTH, HEIGHT);
 	}
-	
+
 	public void addBoundary(Shape s) {
 		borders.add(s);
 	}
