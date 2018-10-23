c6aa7d72560fe0622d26b5b8ea851867349de042
garnacpj <garnacpj@garnacpj-5520.rose-hulman.edu>
2018-10-22 21:44:04 -0400

    temp

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import util.Random;
+
+/**
+ * 
+ * The starting point of the Bubble Bobble Game
+ * 
+ * Created for the CSSE220 Final Project
+ * 
+ * @author garnacpj
+ *
+ */
+
+public class Main {
+	private static final int WIDTH = 1000;
+	private static final int HEIGHT = 1000;
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	
+	
+	
+}
